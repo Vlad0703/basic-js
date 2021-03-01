b038bf1338e20f1867c794e2f084b5421ff9ba10
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-03-01 13:28:15 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,15 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample (input) {
+    if (typeof(input) === 'string') {
+    if (input == '' || input == ' ' || isNaN(Number(input)) || Number(input) == 0 ||typeof(Number(input)) != 'number'){
+      return false;
+    } else {
+      return Math.ceil( Math.log( MODERN_ACTIVITY / Number(input) ) / (0.693/HALF_LIFE_PERIOD) )
+    }
+  } else {
+    return false;
+  }
 
 }
 
