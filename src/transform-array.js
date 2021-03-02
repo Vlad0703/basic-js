50c458e91022c476427071c0a522635334417f0e
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-03-02 15:12:29 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+    throw new Error('input is not array')
+  }
+  let inp = arr;
+  let last_itt;
+  for (let el of inp){
+    if (typeof el == 'string'){
+      switch (el) {
+        case '--discard-next':
+          if (inp.indexOf(el) + 1 < inp.length) {
+            inp.splice(inp.indexOf(el) + 1, 1);
+          break;
+          } else {
+           break;
+          }
+        case '--discard-prev':
+          if ((inp.indexOf(el) - 1 != -1) && (typeof inp[inp.indexOf(el) - 1] != 'string')) {
+            inp.splice(inp.indexOf(el) - 1, 1);
+            break;
+          } else {
+            break;
+          }
+        case '--double-next':
+          if (inp.indexOf(el) + 1 < inp.length) {
+            inp.splice(inp.indexOf(el) +1, 0, inp[inp.indexOf(el) + 1 ]);
+          break;
+          } else {
+           break;
+          };
+          case '--double-prev':
+            if ((inp.indexOf(el) - 1 != -1) && (typeof inp[inp.indexOf(el) - 1] != 'string')) {
+              inp.splice( inp.indexOf(el) , 1, inp[inp.indexOf(el) - 1 ]);
+              break;
+            } else {
+              break;
+            }
+      }
+    }
+  }
+  
+  for (let i = 0; i < inp.length; ) {
+    if (typeof inp[i] == 'string') {
+      inp.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return inp;
 };
